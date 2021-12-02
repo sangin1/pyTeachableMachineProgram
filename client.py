362e1033943a23cfbc0bf41980deee94aab53b3d
sangin1 <tkwk9gh@naver.com>
2021-12-02 11:30:47 +0900

    클라이언트 소켓 구현

--- client.py
+++ client.py
@@ -1,18 +1,36 @@
+import tkinter.messagebox
 from tkinter import *
 from PIL import Image,ImageTk
 from tkinter import filedialog
 import PIL
+import socket
+import numpy
 
 def open():
-    global photo # 함수에서 이미지를 기억하도록 전역변수 선언 (안하면 사진이 안보임)
+    global photo
     filename = filedialog.askopenfilename(initialdir='', title='파일선택', filetypes=(
     ('png files', '*.png'), ('jpg files', '*.jpg')))
     
     image = Image.open(filename)
     image = image.resize((224,224),PIL.Image.ANTIALIAS)
     photo = ImageTk.PhotoImage(image)
-    Label(root,image=photo).pack() 
- 
+    Label(root,image=photo).pack()
+
+    sock.send(photo.encode())
+
+    a = sock.recv(1024).decode()
+    water = a.split('-')[0]
+    melon = a.split('-')[1]
+
+    Label(root,text="수박"+water+"% 일치").pack()
+    Label(root,text="멜론"+water+"% 일치").pack()
+    
+def on_closing():
+    socket.close()
+    root.destroy()
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.connect(('localhost',5000));
 
 root = Tk()
 
@@ -26,4 +44,5 @@ label1.pack()
 my_btn = Button(root, text='파일열기', command=open).pack()
 
 
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
